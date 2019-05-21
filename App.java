4fd0181fedc9b06fc09fa76bf868a585bbb3c08d
rajesh8179 <36216174+rajesh8179@users.noreply.github.com>
2019-05-21 06:40:37 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+for(int i=1;i<=4;i++){
+        System.out.println( "Hello World!" );
+System.out.println( "miss u" );
+System.out.println( "love u" );
+System.out.println( "jaanu" );
+ }
+    }
+}
